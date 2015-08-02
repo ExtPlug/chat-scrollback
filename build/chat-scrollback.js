7319444eba7be151de5c00b3c93827db0bb7320e
René Kooi <rene@kooi.me>
2015-08-02 21:21:01 +0200

    0.1.0

--- build/chat-scrollback.js
+++ build/chat-scrollback.js
@@ -9,6 +9,7 @@ define('extplug/chat-scrollback/main',['require','exports','module','extplug/Plu
   var _require = require('underscore');
 
   var defer = _require.defer;
+  var debounce = _require.debounce;
 
   var $ = require('jquery');
 
@@ -19,101 +20,107 @@ define('extplug/chat-scrollback/main',['require','exports','module','extplug/Plu
   var LIMIT = 512;
 
   var ChatScrollback = Plugin.extend({
-    name: 'Chat Scrollback',
-    description: 'Remembers chat messages from earlier sessions.',
+    name: 'Chat Scrollback (Experimental)',
+    description: 'Remembers chat messages from earlier sessions. Works in up to 5 rooms.',
 
     enable: function enable() {
-      return this.disable();
-
-      this.chat = this.load();
-
-      Events.on('chat:receive', this.onReceive, this);
-      Events.on('chat:delete', this.onDelete, this);
-      Events.on('room:joined', this.onJoined, this);
+      this.listenTo(Events, 'chat:receive', this.onReceive);
+      this.listenTo(Events, 'chat:delete', this.onDelete);
+      this.listenTo(Events, 'room:joined', this.onJoined);
       if (currentRoom.get('joined')) {
         this.onJoined();
       }
+
+      this.saveSometimes = debounce(this.save.bind(this), 200);
+
+      API.chatLog('The Chat Scrollback plugin is experimental, and might not always function properly. If you\'re having issues using plug.dj, try disabling the Scrollback plugin!');
     },
 
     disable: function disable() {
-      this.save(this.chat);
-      Events.off('chat:receive', this.onReceive);
-      Events.off('chat:delete', this.onDelete);
-      Events.off('room:joined', this.onJoined);
-
+      this.save();
       this.chat = null;
     },
 
-    onReceive: function onReceive(msg) {
-      // ignore messages that were triggered by the plugin
-      if (!msg.epReceived && msg.type !== 'welcome') {
-        msg.epReceived = Date.now();
-        msg.epSlug = location.pathname.slice(1);
-        this.chat.push(msg);
-        if (this.chat.length > this.settings.get('limit')) {
-          this.chat.shift();
-        }
-        this.save(this.chat);
-      }
+    onReceive: function onReceive() {
+      this.chat = $('#chat-messages').html();
+      this.saveSometimes();
     },
-
-    // mark deleted messages as such in the scrollback history
-    onDelete: function onDelete(cid, prop) {
-      // this is triggered by the plugin
-      if (prop === REDELETE) {
-        return;
-      }
-      this.chat.some(function (msg) {
-        if (msg.cid === cid) {
-          msg.epDeleted = true;
-          // break
-          return true;
-        }
-      });
+    onDelete: function onDelete() {
+      this.chat = $('#chat-messages').html();
+      this.saveSometimes();
     },
 
     load: function load() {
       try {
         var chat = JSON.parse(localStorage.getItem(LS_KEY));
+        // old-style scrollback storage
         if (Array.isArray(chat)) {
-          return chat;
+          localStorage.setItem(LS_KEY, '');
+          chat = {};
+        }
+        if (typeof chat === 'object') {
+          return chat[currentRoom.get('slug')] || '';
         }
       } catch (e) {}
       return [];
     },
-    save: function save(chat) {
-      localStorage.setItem(LS_KEY, JSON.stringify(chat));
+    save: function save() {
+      var ls = undefined;
+      try {
+        ls = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+      } catch (e) {
+        ls = {};
+      }
+      ls[currentRoom.get('slug')] = {
+        chat: this.chat,
+        time: Date.now()
+      };
+
+      this.prune(ls);
+
+      localStorage.setItem(LS_KEY, JSON.stringify(ls));
+    },
+
+    // keeps only the 5 most recent caches
+    prune: function prune(ls) {
+      var keys = Object.keys(ls);
+      if (keys.length > 5) {
+        keys.sort(function (a, b) {
+          if (ls[a].time > ls[b].time) return -1;
+          if (ls[a].time < ls[b].time) return 1;
+          return 0;
+        });
+        keys.slice(5).forEach(function (key) {
+          delete ls[key];
+        });
+      }
+    },
+
+    insertScrollback: function insertScrollback(scrollback) {
+      $('#chat-messages').prepend(scrollback);
+      return;
+      // insert old chat messages _before_ new ones
+      var newMessages = $('#chat-messages').children().remove();
+      scrollback.forEach(function (msg) {
+        Events.trigger('chat:receive', msg);
+        if (msg.epDeleted) {
+          defer(function () {
+            return Events.trigger('chat:delete', msg.cid, REDELETE);
+          });
+        }
+      });
+      $('#chat-messages').append(newMessages);
     },
 
     onJoined: function onJoined() {
-      clearTimeout(this.timeout);
-      var slug = location.pathname.slice(1);
-      var chat = this.chat.filter(function (msg) {
-        return msg.epSlug === slug && msg.epReceived < Date.now() - 1;
-      });
-      // wait for other plugins to register
-      this.timeout = setTimeout(function () {
-        // insert old chat messages _before_ new ones
-        var newMessages = $('#chat-messages').children().remove();
-        chat.forEach(function (msg) {
-          Events.trigger('chat:receive', msg);
-          if (msg.epDeleted) {
-            defer(function () {
-              Events.trigger('chat:delete', msg.cid, REDELETE);
-            });
-          }
-        });
-        $('#chat-messages').append(newMessages);
+      var _this = this;
 
-        if (chat.length > 0) {
-          var last = new Date(chat[chat.length - 1].epReceived);
-          Events.trigger('chat:receive', {
-            type: 'log',
-            classes: 'extplug-scrollback',
-            message: 'Loaded scrollback from ' + last.toLocaleString()
-          });
-        }
-      }, 500);
+      this.chat = (this.load() || {}).chat;
+      if (this.chat) {
+        defer(function () {
+          _this.insertScrollback(_this.chat);
+        });
+      }
     }
 
   });
